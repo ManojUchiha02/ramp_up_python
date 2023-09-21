dfba151c7abc7853a049a97e83f6832628ed8c7c
unknown <manoj.boya@terralogic.com>
2023-09-21 11:14:19 +0530

    Added Task_02.py

---
+++ Task_02.py
@@ -0,0 +1,19 @@
+import random
+import string
+class UsernameGenerator:
+    def __init__(self):
+        self.used_usernames = set()
+    def generate_username(self, length=8):
+        while True:
+            username = ''.join(random.choice(string.ascii_letters + string.digits) for i in range(length))
+            if len(self.used_usernames) == 10:
+                for i in self.used_usernames:
+                    print(f"Unique Usernames:{i}")
+            if username not in self.used_usernames:
+                self.used_usernames.add(username)
+                yield username
+
+generator = UsernameGenerator()
+
+for i in range(12):
+    unique_username = next(generator.generate_username())
